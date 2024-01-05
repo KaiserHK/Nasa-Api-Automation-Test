36c8156c1fe6c4bbb71ddd8fdce11674673bb0ed
KaiserHK <133049212+KaiserHK@users.noreply.github.com>
2024-01-04 19:52:45 -0500

    Implemented TestClass and Tests
    Created 7 tests that test the NASA Asteroids NeoWs API. The tests check the status code of the response under varying request conditions.
    
    Currently, 5 of the 7 tests pass, with the 4th and 5th tests failing. These tests fail due to the status code not being what is expected. The response I get is a 400 Bad Request error. I guess I am somehow malforming the request in some way.

--- Nasa-Api-Automation-Test/tests.py
+++ Nasa-Api-Automation-Test/tests.py
@@ -3,56 +3,85 @@ from test_requests import make_request
 
 class TestClass:
     
-    def SearchWithSuccess():
+    def test_SearchWithSuccess(self):
         #Arrange
+        key = "api_key=DEMO_KEY";
+        
         #Act
+        response = make_request(key);
+        
         #Assert
-        pass;
-    def SearchWithoutQueryParameters():
+        assert response.status_code == 200;
+        data = response.json();
+        assert len(data) > 0;
+        assert data["element_count"] > 0;
+        
+    def test_SearchWithoutQueryParameters(self):
         #Arrange
+        query = "";
+        
         #Act
+        response = make_request(query);
+        
         #Assert
-        pass;
-    def SearchWithStartDateOnly():
+        assert response.status_code == 403;
+
+    def test_SearchWithStartDateOnly(self):
         #Arrange
+        key = "api_key=DEMO_KEY";
+        startDate = "start_date=2023-01-01";
+        query = startDate + "&" + key;
+        
         #Act
+        response = make_request(query);
+        
         #Assert
-        pass;
-    def SearchWithEndDateOnly():
+        assert response.status_code == 200;
+        
+    def test_SearchWithEndDateOnly(self):
         #Arrange
+        key = "api_key=DEMO_KEY";
+        endDate = "end_date=2000-01-01";
+        query = endDate + "&" + key;
+        
         #Act
+        response = make_request(query);
+        
         #Assert
-        pass;
-    def SearchWithValidDateRange():
+        assert response.status_code == 200;
+        
+    def test_SearchWithValidDateRange(self):
         #Arrange
+        key = "api_key=DEMO_KEY";
+        startDate = "start_date=2022-01-01";
+        endDate = "end_date=2023-01-01";
+        query = startDate + "&" + endDate + "&" + key;
+        
         #Act
+        response = make_request(query);
+        
         #Assert
-        pass;
-    def SearchWithInvalidDateRange():
+        assert response.status_code == 200;
+        
+    def test_SearchWithInvalidDateRange(self):
         #Arrange
+        key = "api_key=DEMO_KEY";
+        startDate = "start_date=2000-01-01";
+        endDate = "end_date=1999-01-01";
+        query = startDate + "&" + endDate + "&" + key;
+        
         #Act
+        response = make_request(query);
+        
         #Assert
-        pass;
-    def SearchWithInvalidApiToken():
+        assert response.status_code == 400;
+        
+    def test_SearchWithInvalidApiToken(self):
         #Arrange
+        key = "api_key=SOME_TOKEN";
+        
         #Act
+        response = make_request(key);
+        
         #Assert
-        pass;
-
-
-
-def test_search_asteroids_with_success():
-    #Arrange
-    api = "api_key=DEMO_KEY"
-
-    #Act
-    response = make_request(api)
-
-    #Assert
-    assert response.status_code == 200
-    data = response.json()
-    assert len(data) > 0
-    assert data["element_count"] > 0
-    print(data)
-
-
+        assert response.status_code == 403;
